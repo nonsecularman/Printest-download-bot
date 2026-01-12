8f1027341db4fb72fbb782dcddde856fc41c2aec
nonsecularman <krishbharti3404@gmail.com>
2026-01-12 13:00:18 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,9 +1,18 @@
-# cache.py
+import time
 
 _cache = {}
 
-def get_cache(key, default=None):
-    return _cache.get(key, default)
+def get_cache(key):
+    data = _cache.get(key)
+    if not data:
+        return None
 
-def set_cache(key, value):
-    _cache[key] = value
+    value, expiry = data
+    if time.time() > expiry:
+        _cache.pop(key, None)
+        return None
+
+    return value
+
+def set_cache(key, value, ttl):
+    _cache[key] = (value, time.time() + ttl)
