11c1be8353fd9223cc318fb45c89498fbc3e50ae
nonsecularman <krishbharti3404@gmail.com>
2026-01-12 12:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("8367158201:AAF6x9RGVYzYTx672GI_0iBipuAbExKeMXM")
+BOT_TOKEN = os.getenv("8367158201:AAHxxL5aWZYPZhHLYwi38PcX-P5YxVZ1lqs")
 OWNER_ID = int(os.getenv("7519668949", 0))
 CACHE_TIME = 3600  # 1 hour
 RATE_LIMIT = 3     # requests
